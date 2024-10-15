88c24a815632644c7325f97a6fe91fa3ee1f9d72
Teeque87 <teeque87@gmail.com>
2024-10-15 14:22:43 +0200

    initial db class

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class Database():
+    __DB_LOCATION = "database.db"
+
+    def __init__(self):
+        """initialize db class variables and set cursor"""
+        self.connection = sqlite3.connect(Database.__DB_LOCATION)
+        self.cur = self.connection.cursor()
+
+    def __del__(self):
+        self.connection.close()
+
+    def get_all_from_db(self):
+        self.cur.execute('''SELECT * FROM articles''')
+
+    def add_to_database(self, data):
+        """add new entry (int: product_id, string: name, int: amount) to the database and replace entry if already exists (product_id)"""
+        self.create_table()
+        self.cur.executemany('REPLACE INTO articles VALUES(?, ?, ?)', data)
+        self.connection.commit()
+
+    def create_table(self):
+        """create a database table if it does not exist already"""
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS articles(product_id integer PRIMARY KEY, name text, amount integer)''')
+        self.connection.commit()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_value):
+        """looking for exceptions before exiting and closing the connection"""
+        self.cursor.close()
+        if isinstance(exc_value, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+        self.connection.close()
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from db import Database
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    db = Database()
+    db.add_to_database((29817, "Schraube M8x40", 489))
+    db.add_to_database((29818, "Schraube M6x40", 378))
+    db.add_to_database((29389, "Schraube M10x40", 154))
+    db.add_to_database((22929, "Schraube M6x40", 543))
+    db.add_to_database((29303, "Trennscheiben", 142))
+    db.add_to_database((22827, "Glühbirne 60W", 242))
+    db.add_to_database((10088, "Muttern M8", 562))
