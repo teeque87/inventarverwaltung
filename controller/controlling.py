ebae734cbbc3c61fc97957dc0a16681c579b4451
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-17 00:05:18 +0200

    gelöscht und alle Funktionen zum Starten des Programms in die main überführt

--- controller/controlling.py
+++
@@ -1,6 +0,0 @@
-import services.item_services
-import logging
-
-# Erstelle die Datenbank-Instanz einmal, um sie von außen übergeben zu können
-db_instance = Database()  # Erstelle die Datenbank-Instanz einmal
-item_service = ItemServices(db_instance)
