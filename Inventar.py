fdaff68d2c15937c23569de3b3c5dcfb7826ca02
unknown <chrissi9009@cgaaex1@gmail.com>
2024-10-16 11:33:22 +0200

    Test

--- Inventar.py
+++ Inventar.py
@@ -1,4 +1,8 @@
 # Inventar.py
 
 class Inventar:
-    def __init__(self,
+    def __init__(self, product_id, name, amount, cat_id):
+        self.product_id = product_id
+        self.name = name
+        self.amount = amount
+        self.category = cat_id
