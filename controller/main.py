e4e85df37d3a2b597969cb38061253c80cd076e9
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-17 00:08:03 +0200

    Datenbank -Instanz erzeugt und Funktionen zur Abfrage alle Artikel und ein Artikel hinzugefügt

--- controller/main.py
+++ controller/main.py
@@ -1,13 +1,25 @@
 from model.item import Item
+from services.item_services import ItemRepository
 from services.item_services import ItemServices
+from repo.db import Database
+import logging
+
 
 def main():
-    item1 = Item(13, "Holzschraube", 350, 3)
-    print(item1)
+    product_id = 1 # Eingabe aus UI
+
+    # Erstelle die Datenbank-Instanz einmal, um sie dann von außen übergeben zu können
+    db_instance = Database()  # Erstelle die Datenbank-Instanz einmal
+    item_repository = ItemRepository(db_instance)
+
+    item_services =  ItemServices(item_repository)
+    all_items = item_services.fetch_all_items()
+    one_item = item_services.fetch_one_item(product_id) # für Einzelartikel-Ausgabe - UI Eingabe+Verknüpfung
+    for item in all_items:
+        print(item)
+
 
 if __name__ == "__main__":
     main()
 
-# Erstelle die Datenbank-Instanz einmal, um sie dann von außen übergeben zu können
-db_instance = Database()  # Erstelle die Datenbank-Instanz einmal
-item_service = ItemServices(db_instance)
+
