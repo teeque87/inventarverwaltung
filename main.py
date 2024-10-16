f0d98e9004bd0a3503f4d694d52ac6a5f8c57ca2
Teeque87 <teeque87@gmail.com>
2024-10-16 12:02:58 +0200

    fixed folder structure

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from db import Database
-
 if __name__ == "__main__":
-    # initialize Database
-    db = Database()
-
-
-
-    # fetch all data from database
-    res = db.get_all_from_db()
-
-    
-    print()
+    print('Programm wird gestartet')
