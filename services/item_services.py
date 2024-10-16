27c4e7b8e312da2f06f368881c1628097d6f3d08
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-17 00:06:16 +0200

    Datenbank als Klasse initialisiert, weitere Abfragefunktionen hinzugefügt

--- services/item_services.py
+++ services/item_services.py
@@ -1,11 +1,5 @@
-import os
-import sys
 from model.item import Item
 from repo.db import Database
-
-
-# insert root directory into python module search path
-sys.path.insert(1, os.getcwd())
 
 # initialisiert die ItemServices in einer Datenbankinstanz
 class ItemRepository:
@@ -20,14 +14,6 @@ class ItemRepository:
     #except sqlite3.Error as e:
         #print(f"Allgemeiner Datenbankfehler: {e}")
 
-    # Datenbanklogik einfügen
-    #def get_item(self, item_id):
-        #pass
-
-    def fetch_all(self):
-        return self.db.fetch_all()
-
-
 # diese Klasse stellt alle Services zur Verwaltung der Artikel bereit
 class ItemServices:
 
@@ -35,17 +21,19 @@ class ItemServices:
         self.repository = repository
 
     def fetch_one_item(self, product_id):
-        item = self.repository.get_item(product_id)
+        item = self.repository.db.fetch_one(product_id)
         product_id, name, amount, cat_id = item
         print(Item(product_id, name, amount, cat_id))
 
     def fetch_all_items(self):
         try:
             # Daten aus Datenbank abfragen
-            fetched_data = self.repository.fetch_all()
+            fetched_data = self.repository.db.fetch_all()
+
             # Liste von Artikeln anlegen und zurückgeben
             items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in fetched_data]
             return items
+            #return fetched_data
 
         except Exception as e:
             print(f"Fehler beim Abrufen der Artikel: {e}")
