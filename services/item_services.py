fc95e22f9d60836e37738cb5f102874f7a090c5c
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-17 12:54:00 +0200

    try/except Blöck gelöscht, da er in db direkt erfolgen soll

--- services/item_services.py
+++ services/item_services.py
@@ -5,14 +5,7 @@ from repo.db import Database
 class ItemRepository:
     def __init__(self, db: Database):
         self.db = db  #Instanz der Klasse Database zur Datenbankinteraktion
-
-    #try:
-        #self.db = sqlite3.connect(Database)
-        #print("Erfolgreich mit der Datenbank verbunden!")
-    #except sqlite3.OperationalError as e:
-        #print(f"Fehler beim Verbinden mit der Datenbank: {e}")
-    #except sqlite3.Error as e:
-        #print(f"Allgemeiner Datenbankfehler: {e}")
+        # gibt es einen try/except Block in der Datenbank?
 
 # diese Klasse stellt alle Services zur Verwaltung der Artikel bereit
 class ItemServices:
@@ -23,7 +16,8 @@ class ItemServices:
     def fetch_one_item(self, product_id):
         item = self.repository.db.fetch_one(product_id)
         product_id, name, amount, cat_id = item
-        print(Item(product_id, name, amount, cat_id))
+        return (Item(product_id, name, amount, cat_id)) #kann durch print ersetzt werden, falls direkte Ausgabe rfolgen soll
+        #return item
 
     def fetch_all_items(self):
         try:
@@ -33,9 +27,9 @@ class ItemServices:
             # Liste von Artikeln anlegen und zurückgeben
             items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in fetched_data]
             return items
-            #return fetched_data
+
 
         except Exception as e:
             print(f"Fehler beim Abrufen der Artikel: {e}")
-            # None wird bei Fehler zurückgegeben
-            return None
+            # [] wird bei Fehler zurückgegeben
+            return []
