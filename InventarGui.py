62f1c5d495f833557b349a8f7d2da74a8dddd2b6
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-23 00:38:14 +0200

    vInventarGui added new functions

--- InventarGui.py
+++ InventarGui.py
@@ -1,5 +1,6 @@
+import tkinter
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox # Modul stellt Dialogboxen für verschiedene Meldungen zur Verfügung (z.B. in confirm_delete oder als Exception Anzeige)
 from services.item_services import ItemServices
 
 
@@ -7,7 +8,7 @@ class InventarGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Inventarverwaltung")
-        self.root.geometry("400x300") # wenn vom deafult abgewichen werden soll, Größeneingabe
+        self.root.geometry("400x400") # wenn vom default abgewichen werden soll, Größeneingabe
         self.item_services = ItemServices() # Verbindung zu ItemServices (Controller)
         self.create_main_menu() # Hauptmenü
 
@@ -23,14 +24,14 @@ class InventarGUI:
         options = [
             ("Artikel nach ID suchen", self.search_by_id),
             ("Artikel nach Name suchen", self.search_by_name),
-            ("Artikel hinzufügen", self.add_article),
-            ("Artikel bearbeiten", self.edit_article),
+            ("Artikel hinzufügen", self.add_item),
+            ("Artikel bearbeiten", self.edit_article_by_id),
             ("Artikel löschen", self.delete_article),
             ("Inventurliste anzeigen", self.display_inventory_list),
             ("Beenden", self.quit_program)
         ]
 
-        for i, (text, command) in enumerate(options):
+        for i, (text, command) in enumerate(options): # enumerate erzeugt Index i als auch jeweils Tupel-Paar in Liste (text, command)
             tk.Label(frame, text=text).grid(row=i+2, column=0, sticky="w", padx=10)
             btn_select = tk.Button(frame, text="Wählen", command=command)
             btn_select.grid(row=i+2, column=1, pady=5)
@@ -40,8 +41,7 @@ class InventarGUI:
         frame = tk.Frame(self.root)
         frame.pack(pady=20)
 
-        btn_search = tk.Button(frame, text="Suchen",
-                           command=lambda: self.display_item(self.item_services.search_items_name(entry_id.get())))
+        btn_search = tk.Button(frame, text="Suchen", command=lambda: self.display_item(self.item_services.search_items_name(entry_id.get())))
         btn_search.grid(row=2, column=0, columnspan=2, pady=10)
 
         btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
@@ -67,7 +67,7 @@ class InventarGUI:
         entry_id = tk.Entry(frame)
         entry_id.grid(row=1, column=1)
 
-        btn_search = tk.Button(frame, text="Suchen", command=lambda: self.display_item(self.item_services.search_items_id(entry_id.get())))
+        btn_search = tk.Button(frame, text="Suchen", command=lambda: self.handle_search_by_id((int(entry_id.get()))))
         btn_search.grid(row=2, column=0, columnspan=2, pady=10)
 
         btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
@@ -75,43 +75,48 @@ class InventarGUI:
 
     def handle_search_by_id(self, product_id):
         try:
-            item = self.item_services.search_items_id(product_id)
-            self.display_item(item)
+            all_items = self.item_services.search_items_id(product_id)
+            for item in all_items:
+                # print(item)
+                self.display_item(item)
         except Exception as e:
             messagebox.showerror("Fehler", f"Ein Fehler ist aufgetreten: {e}")
 
     def display_item(self, item):
         if item:
-            messagebox.showinfo("Artikel gefunden", f"ID: {item.product_id}, Name: {item.name}, Menge: {item.amount}, Kategorie: {item.cat_id}")
+            messagebox.showinfo("Artikel gefunden", f"ID: {item.product_id}, Name: {item.name}, Menge: {item.amount}, Kategorie: {item.category}")
         else:
             messagebox.showwarning("Nicht gefunden", "Kein Artikel mit dieser ID gefunden.")
 
-    def add_article(self):
+    def add_item(self):
         self.clear_frame()
-
         frame = tk.Frame(self.root)
         frame.pack(pady=20)
 
         label = tk.Label(frame, text="Neuen Artikel hinzufügen", font=("Helvetica", 14))
         label.grid(row=0, column=0, columnspan=2)
 
-        tk.Label(frame, text="Artikelname: ").grid(row=1, column=0)
+        tk.Label(frame, text="Artikel ID: ").grid(row=1, column=0)
+        entry_id = tk.Entry(frame)
+        entry_id.grid(row=1, column=1)
+
+        tk.Label(frame, text="Artikelname: ").grid(row=2, column=0)
         entry_name = tk.Entry(frame)
-        entry_name.grid(row=1, column=1)
+        entry_name.grid(row=2, column=1)
 
-        tk.Label(frame, text="Menge: ").grid(row=2, column=0)
+        tk.Label(frame, text="Menge: ").grid(row=3, column=0)
         entry_amount = tk.Entry(frame)
-        entry_amount.grid(row=2, column=1)
+        entry_amount.grid(row=3, column=1)
 
-        tk.Label(frame, text="Kategorie-ID: ").grid(row=3, column=0)
+        tk.Label(frame, text="Kategorie-ID: ").grid(row=4, column=0)
         entry_category = tk.Entry(frame)
-        entry_category.grid(row=3, column=1)
+        entry_category.grid(row=4, column=1)
 
-        btn_add = tk.Button(frame, text="Hinzufügen", command=lambda: self.add_new_item(entry_name.get(), entry_amount.get(), entry_category.get()))
-        btn_add.grid(row=4, column=0, columnspan=2, pady=10)
+        btn_add = tk.Button(frame, text="Hinzufügen", command=lambda: self.item_services.add_new_item(int(entry_id.get()), entry_name.get(), int(entry_amount.get()), int(entry_category.get())))
+        btn_add.grid(row=5, column=0, columnspan=2, pady=10)
 
         btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
-        btn_back.grid(row=5, column=0, columnspan=2, pady=5)
+        btn_back.grid(row=6, column=0, columnspan=2, pady=5)
 
     def add_article_to_db(self, name, amount, category):
         if self.item_services.add_new_item(name, amount, category):
@@ -119,16 +124,78 @@ class InventarGUI:
         else:
             messagebox.showerror("Fehler", "Fehler beim Hinzufügen des Artikels.")
 
-    def edit_article(self):
-        try:
-            if amount < 0:
-                raise ValueError("Die Menge muss größer oder gleich 0 sein.")
+    def edit_article_by_id(self):
+        self.clear_frame()
+        frame = tk.Frame(self.root)
+        frame.pack(pady=20)
 
-            self.item_services.add_new_item((product_id, name, amount, cat_id))
-            return True  # Rückgabe, um den Erfolg zu kennzeichnen
+        label = tk.Label(frame, text="Artikel nach ID suchen", font=("Helvetica", 14))
+        label.grid(row=0, column=0, columnspan=2)
+
+        tk.Label(frame, text="Artikel-ID: ").grid(row=1, column=0)
+        entry_id = tk.Entry(frame)
+        entry_id.grid(row=1, column=1)
+
+        btn_search = tk.Button(frame, text="Suchen", command=lambda: self.handle_edit_by_id((int(entry_id.get()))))
+        btn_search.grid(row=2, column=0, columnspan=2, pady=10)
+
+        btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
+        btn_back.grid(row=3, column=0, columnspan=2, pady=5)
+
+        #try:
+            #if amount < 0:
+                #raise ValueError("Die Menge muss größer oder gleich 0 sein.")
+
+            #self.item_services.add_new_item((product_id, name, amount, cat_id))
+            #return True  # Rückgabe, um den Erfolg zu kennzeichnen
+       # except Exception as e:
+            #print(f"Fehler beim Aktualisieren des Artikels: {e}")
+           # return False  # Rückgabe bei Fehlern
+
+    def handle_edit_by_id(self, product_id):
+        try:
+            all_items = self.item_services.search_items_id(product_id)
+            for item in all_items:
+                print(item)
+                self.edit_item(item)
         except Exception as e:
-            print(f"Fehler beim Aktualisieren des Artikels: {e}")
-            return False  # Rückgabe bei Fehlern
+            messagebox.showerror("Fehler", f"Ein Fehler ist aufgetreten: {e}")
+
+    def edit_item(self, item):
+        self.clear_frame()
+        self.item = item
+        frame = tk.Frame(self.root)
+        frame.pack(pady=20)
+
+        label = tk.Label(frame, text="Artikel ändern", font=("Helvetica", 14))
+        label.grid(row=0, column=0, columnspan=2)
+
+        tk.Label(frame, text="Artikel ID: ").grid(row=1, column=0)
+        entry_id = tk.StringVar()
+        entry_id.set(str(self.item.product_id))
+        entry_id = tk.Entry(frame, textvariable=entry_id)
+        #entry_id.insert(str(self.item.product_id))
+        entry_id.grid(row=1, column=1)
+
+        tk.Label(frame, text="Artikelname: ").grid(row=2, column=0)
+        entry_name = tk.Entry(frame)
+        entry_name.grid(row=2, column=1)
+
+        tk.Label(frame, text="Menge: ").grid(row=3, column=0)
+        entry_amount = tk.Entry(frame)
+        entry_amount.grid(row=3, column=1)
+
+        tk.Label(frame, text="Kategorie-ID: ").grid(row=4, column=0)
+        entry_category = tk.Entry(frame)
+        entry_category.grid(row=4, column=1)
+
+        btn_add = tk.Button(frame, text="Hinzufügen",
+                            command=lambda: self.item_services.add_new_item(int(entry_id.get()), entry_name.get(), int(entry_amount.get()), int(entry_category.get())))
+        btn_add.grid(row=5, column=0, columnspan=2, pady=10)
+
+        btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
+        btn_back.grid(row=6, column=0, columnspan=2, pady=5)
+
 
     def delete_article(self):
         self.clear_frame()
@@ -143,18 +210,18 @@ class InventarGUI:
         entry_id = tk.Entry(frame)
         entry_id.grid(row=1, column=1)
 
-        btn_delete = tk.Button(frame, text="Löschen", command=lambda: self.confirm_delete(entry_id.get()))
+        btn_delete = tk.Button(frame, text="Löschen", command=lambda: self.confirm_delete(int(entry_id.get())))
         btn_delete.grid(row=2, column=0, columnspan=2, pady=10)
 
         btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
         btn_back.grid(row=3, column=0, columnspan=2, pady=5)
 
-    def confirm_delete(self, product_id):
-        """Fragt den Benutzer, ob er den Artikel wirklich löschen möchte."""
-        if messagebox.askyesno("Bestätigung", "Möchten Sie diesen Artikel wirklich löschen?"):
+    def confirm_delete(self, product_id): # fragt Benutzer nach Bestätigung für das Löschen ine einem extra Fenster
+
+        if messagebox.askyesno("Bestätigung", "Möchten Sie diesen Artikel wirklich löschen?"): # askyesno-Methode gibt ja=true und nein=false zurück
             try:
                 product_id = int(product_id)
-                if self.item_services.delete_article(product_id):
+                if self.item_services.delete_item(product_id):
                     messagebox.showinfo("Erfolg", "Artikel erfolgreich gelöscht.")
                 else:
                     messagebox.showerror("Fehler", "Fehler beim Löschen des Artikels.")
@@ -169,19 +236,22 @@ class InventarGUI:
         frame = tk.Frame(self.root)
         frame.pack(pady=20)
 
-        label = tk.Label(frame, text="Inventurliste", font=("Helvetica", 14))
+        label = tk.Label(frame, text="Inventarliste", font=("Helvetica", 14))
         label.pack(pady=10)
 
-        inventory_list = self.item_services.fetch_all_items()
+        inventory_list = self.item_services.get_all_items()
         text_area = tk.Text(frame, wrap="word", height=15, width=60)
         text_area.pack(pady=10)
 
         for item in inventory_list:
-            text_area.insert(tk.END, f"ID: {item.product_id}, Name: {item.name}, Menge: {item.amount}, Kategorie: {item.cat_id}\n")
+            print(item)
+            text_area.insert(tk.END, f"ID: {item.product_id}, Name: {item.name}, Menge: {item.amount}, Kategorie: {item.category}\n")
         text_area.config(state=tk.DISABLED)
+        text_area.pack()
 
         btn_back = tk.Button(frame, text="Zurück", command=self.create_main_menu)
         btn_back.pack(pady=5)
+        frame.pack()
 
     def quit_program(self):
         self.root.quit()
@@ -202,3 +272,5 @@ if __name__ == "__main__":
    root = tk.Tk()
    app = InventarGUI(root)
    root.mainloop()
+
+# bei ItemServices muss in Zeile 139 bei delete_item noch der return mit True gesetzt werden, sonst gibt es einen Fehler
--- services/item_services.py
+++ services/item_services.py
@@ -1,74 +1,290 @@
+# Import the Item model and the Database
 from model.item import Item
 from repo.db import Database
 
-# initialisiert die ItemServices in einer Datenbankinstan
+
 class ItemServices:
 
     def __init__(self):
+        # Initialize the Database as db variable inside the ItemServices class
         self.db = Database()
 
-    def get_one_item(self, product_id):
-        item = self.db.fetch_one(product_id)
-        product_id, name, amount, cat_id = item
-        return (Item(product_id, name, amount, cat_id)) #kann durch print ersetzt werden, falls direkte Ausgabe erfolgen soll
+    def get_one_item(self, product_id: int) -> list[tuple]:
+        """
+        Retrieves one item from the database based on the provided product ID.
 
-    def get_all_items(self):
+        Args:
+            product_id (int): The ID of the product to retrieve.
+
+        Returns:
+            list[tuple]: A list containing the item details in a tuple if found, 
+            otherwise an empty list.
+
+        Raises:
+            Exception: If an error occurs while fetching the item from the database.
+        """
         try:
-            # Daten aus Datenbank abfragen
+            # fetch one item from the database
+            item = self.db.fetch_one(product_id)
+            product_id, name, amount, cat_id = item
+            return (Item(product_id, name, amount, cat_id))
+        except Exception as e:
+            print(f"Fehler beim Abrufen eines Artikel: {e}")
+            return []
+
+    def get_all_items(self) -> list[tuple]:
+        """
+        Retrieves all items from the database.
+
+        Returns:
+            list[tuple]: A list of tuples, each containing details of an item 
+            (product_id, name, amount, cat_id).
+
+        Raises:
+            Exception: If an error occurs while fetching the items from the database.
+        """
+        try:
+            # fetch all items from the database
             fetched_data = self.db.fetch_all()
             items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in fetched_data]
             return items
-
         except Exception as e:
-            print(f"Fehler beim Abrufen der Artikel: {e}")
+            print(f"Fehler beim Abrufen aller Artikel: {e}")
             return []
-        
-    def get_all_categories(self):
+
+    def get_all_categories(self) -> list[tuple]:
+        """
+        Retrieves all categories from the database.
+
+        Returns:
+            list[tuple]: A list of tuples containing the category data.
+
+        Raises:
+            Exception: If an error occurs while fetching the categories from the database.
+        """
         try:
-            # Daten aus Datenbank abfragen
+            # fetch all categories from the database
             fetched_data = self.db.fetch_all_categories()
             return fetched_data
+        except Exception as e:
+            print(f"Fehler beim Abrufen aller Kategorien: {e}")
+            return []
+
+    def new_category(self, name: str):
+        """
+        Adds a new category to the database.
+
+        Args:
+            name (str): The name of the new category.
+
+        Raises:
+            Exception: If an error occurs while adding the new category to the database.
+        """
+        try:
+            # create a new entry for a category
+            self.db.add_new_category(name)
+            print(f"Kategorie {name} hinzugefügt.")
+        except Exception as e:
+            print(f"Fehler beim erstellen einer neuen Kategorie: {e}")
+
+    def edit_category(self, cat_id: int, new_name: str):
+        """
+        Edits an existing category's name in the database.
+
+        Args:
+            cat_id (int): The ID of the category to edit.
+            new_name (str): The new name for the category.
+
+        Raises:
+            Exception: If an error occurs while updating the category in the database.
+        """
+        try:
+            # send a request to the database to update the name for a category
+            self.db.edit_category((cat_id, new_name))
+            print(f"Kategorie der ID: {cat_id} zu {new_name} geändert.")
+        except Exception as e:
+            print(f"Fehler beim bearbeiten der Kategorie: {e}")
+
+    def delete_category(self, cat_id: int):
+        """
+        Deletes a category from the database based on its ID.
+
+        Args:
+            cat_id (int): The ID of the category to delete.
+
+        Raises:
+            Exception: If an error occurs while deleting the category from the database.
+        """
+        try:
+            # delete a category from the database
+            self.db.delete_category(cat_id)
+            print(f"Kategorie mit der ID: {cat_id} gelöscht.")
+        except Exception as e:
+            print(f"Fehler beim löschen einer Kategorie: {e}")
+
+    def delete_item(self, product_id: int):
+        """
+        Deletes an item from the database based on its product ID.
+
+        Args:
+            product_id (int): The ID of the item to delete.
+
+        Raises:
+            Exception: If an error occurs while deleting the item from the database.
+        """
+        try:
+            # delete an entry from the database
+            self.db.delete_entry(product_id)
+            print(f"Artikel mit der ID: {product_id} gelöscht.")
+            return True  # wichtig für die Löschung in der InventarGui
 
         except Exception as e:
-            print(f"Fehler beim Abrufen der Artikel: {e}")
-            return []
-        
-    def new_category(self, name):
-        self.db.add_new_category(name)
-        print(f"Kategorie {name} hinzugefügt.")
-    
-    def edit_category(self, cat_id, new_name):
-        self.db.edit_category((cat_id, new_name))
-        print(f"Kategorie der ID: {cat_id} zu {new_name} geändert.")
+            print(f"Fehler beim löschen eines Eintrages: {e}")
 
-    def delete_category(self, cat_id):
-        self.db.delete_category(cat_id)
-        print(f"Kategorie mit der ID: {cat_id} gelöscht.")
+    def add_new_item(self, product_id: int, name: str, amount: int, cat_id: int):
+        """
+        Adds a new item to the database.
 
-    def delete_entry(self, product_id):
-        self.db.delete_entry(product_id)
-        print(f"Artikel mit der ID: {product_id} gelöscht.")
+        Args:
+            product_id (int): The product ID of the new item.
+            name (str): The name of the new item.
+            amount (int): The quantity of the new item.
+            cat_id (int): The category ID the item belongs to.
 
-    def add_new_entry(self, product_id, name, amount, cat_id):
-        self.db.add_to_database((product_id, name, amount, cat_id))
-        print(f"Artikel mit der ID {product_id}, Name {name}, Anzahl {amount}, Kategorie {cat_id} zur Datenbank hinzugefügt.")
-
-    def search_items(self, search_term: str):
-        """Sucht in der Datenbank nach Artikeln basierend auf product_id oder name."""
+        Raises:
+            Exception: If an error occurs while adding the new item to the database.
+        """
         try:
-            # Prüfen, ob die Eingabe eine Zahl ist (ID-Suche)
-            if search_term.isdigit():
-                results = self.db.search_by_id(int(search_term))
-            else:
-                # Falls kein Zahl, wird nach dem Namen gesucht
-                results = self.db.search_by_name(search_term)
+            self.db.add_to_database((product_id, name, amount, cat_id))
+            print(
+                f"Artikel mit der ID {product_id}, Name {name}, Anzahl {amount}, Kategorie {cat_id} zur Datenbank hinzugefügt.")
+        except Exception as e:
+            print(f"Fehler beim erstellen eines neuen Artikel: {e}")
 
-            # Falls Ergebnisse vorhanden, Items erstellen
+    def search_items_id(self, search_term: int) -> list[tuple]:
+        """
+        Searches for items in the database by product ID.
+
+        Args:
+            search_term (int): The product ID to search for.
+
+        Returns:
+            list[tuple]: A list of tuples containing the matching items (product_id, name, amount, cat_id).
+
+        Raises:
+            Exception: If an error occurs while searching for items by ID.
+        """
+        try:
+            results = self.db.search_by_id(search_term)
             if results:
                 items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in results]
                 return items
-            else:
-                return []
         except Exception as e:
-            print(f"Fehler bei der Suche: {e}")
-            return []
+            print(f"Fehler beim Abrufen der Artikel: {e}")
+
+    def search_items_name(self, search_term: str) -> list[tuple]:
+        """
+        Searches for items in the database by name.
+
+        Args:
+            search_term (str): The name or part of the name to search for.
+
+        Returns:
+            list[tuple]: A list of tuples containing the matching items (product_id, name, amount, cat_id).
+
+        Raises:
+            Exception: If an error occurs while searching for items by name.
+        """
+        try:
+            results = self.db.search_by_name(search_term)
+            if results:
+                items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in results]
+                return items
+        except Exception as e:
+            print(f"Fehler beim Abrufen der Artikel: {e}")
+
+    def storage_warning(self) -> bool:
+        """
+        Checks for items with low stock in the database and prints a warning if any are found.
+
+        Returns:
+            bool: True if any items are low in stock, False otherwise.
+
+        Raises:
+            Exception: If an error occurs while checking the stock status.
+        """
+        try:
+            results = self.db.check_storage()
+            if results:
+                items = [Item(product_id, name, amount, cat_id) for product_id, name, amount, cat_id in results]
+                print("\n******** WARNUNG ********\nFolgende Artikel sind niedrig im Bestand\n")
+                for item in items:
+                    print(item)
+                return True
+            else:
+                return False
+        except Exception as e:
+            print(f"Fehler beim Abrufen des Status: {e}")
+            return False
+
+    def add_to_stock(self, product_id: int, amount: int):
+        """
+        Adds a specified amount to the stock of a product in the database.
+
+        Args:
+            product_id (int): The ID of the product to add stock to.
+            amount (int): The amount to add to the stock. Must be positive.
+
+        Raises:
+            Exception: If an error occurs while updating the stock in the database.
+            ValueError: If the provided amount is negative.
+        """
+        try:
+            if amount < 0:
+                print("Die Menge muss positiv sein.")
+                return
+
+            # fetch the current amount
+            item = self.get_one_item(product_id)
+
+            if item:
+                new_amount = item.amount + amount
+                self.db.update_item_amount(product_id, new_amount)
+                print(f"Die Menge des Artikels mit der ID {product_id} wurde um {amount} erhöht.")
+            else:
+                print("Artikel nicht gefunden.")
+        except Exception as e:
+            print(f"Fehler beim hinzufügen zum Bestand: {e}")
+
+    def remove_from_stock(self, product_id: int, amount: int):
+        """
+        Removes a specified amount from the stock of a product in the database.
+
+        Args:
+            product_id (int): The ID of the product to remove stock from.
+            amount (int): The amount to remove from the stock. Must be positive and less than the current stock.
+
+        Raises:
+            Exception: If an error occurs while updating the stock in the database.
+            ValueError: If the provided amount is negative or exceeds available stock.
+        """
+        try:
+            if amount < 0:
+                print("Die Menge muss positiv sein.")
+                return
+
+            # fetch the current amount
+            item = self.get_one_item(product_id)
+
+            if item:
+                if amount > item.amount:
+                    print("Nicht genügend Artikel im Bestand.")
+                    return
+
+                new_amount = item.amount - amount
+                self.db.update_item_amount(product_id, new_amount)
+                print(f"Die Menge des Artikels mit der ID {product_id} wurde um {amount} verringert.")
+            else:
+                print("Artikel nicht gefunden.")
+        except Exception as e:
+            print(f"Fehler beim abziehen vom Bestand: {e}")
