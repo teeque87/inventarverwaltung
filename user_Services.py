e37ce65f1aeaac1d543b8730fcde315b8bdbf42e
Olli Schmidt <ollischmidt045@gmail.com>
2024-10-29 15:00:57 +0100

    added functions for user input and encoding

--- user_Services.py
+++ user_Services.py
@@ -12,7 +12,6 @@ class UserServices:
         return user_name,user_password
 
     def create_user(self, user_name: str, user_password: str):
-
         # Salt generieren und Passwort hashen
         salt = bcrypt.gensalt(rounds=15)
         hashed_password = bcrypt.hashpw(user_password.encode('utf-8'), salt).decode('utf-8')
@@ -26,45 +25,45 @@ class UserServices:
         #)
         self.db.add_new_user(user_name, hashed_password)
 
-    def verify_user(self, user_name: str, user_password: str) -> bool:
-        user = self.db.get_user_by_username(user_name) # muss noch implementiert werden
+    def verify_user(self, user_name: str) -> bool:
+        user = self.db.get_user_by_username(user_name)
 
+        #if user is None:
+            #print("Benutzer nicht gefunden.")
+            #return False
+        return user is not None
+
+    def verify_password(self, user_name: str,  user_password: str) -> bool:
+        user = self.db.get_user_by_username(user_name)
         if user is None:
             print("Benutzer nicht gefunden.")
             return False
 
-        # return bcrypt.checkpw(user_password.encode('utf-8'), user['password'].encode('utf-8'))
-
-        password_from_db = user['password']
+        _, password_from_db = user # entpackt das Tupel
 
         # vergleicht eingegebenes Passwort mit dem in der Datenbank abgelegten Passwort
-        password_hashed = bcrypt.hashpw(user_password.encode('utf-8'), password_from_db.encode('utf-8')).decode('utf-8')
-        if password_hashed == password_from_db:
+        if bcrypt.checkpw(user_password.encode('utf-8'), password_from_db.encode('utf-8')):
             print("Erfolgreich eingeloggt.")
             return True
         else:
             print("Login fehlgeschlagen. Überprüfen Sie Ihr Passwort.")
             return False
 
-    def input_user_verification(self, user_name, user_password):
-        #user_name = input("Bitte Benutzernamen eingeben: ")
-        #user_password = input("Bitte Passwort eingeben: ")
-
-        if self.verify_user(user_name, user_password):
-            print("Login erfolgreich!")
+    def input_user_verification(self):
+        user_name, user_password = self.user_input()
+        if self.verify_user(user_name):
+            return self.verify_password(user_name, user_password)
         else:
-            print("Login fehlgeschlagen. Überprüfen Sie Ihren Benutzernamen oder Ihr Passwort.")
+            print("Benutzer existiert nicht.")
+            return False
 
 
 # Beispiel, hinterher rauslöschen
 if __name__ == "__main__":
 
     user_service = UserServices()
-    user = user_service.user_input()
-    print(user)
-    # Beispiel: Benutzer anlegen
-    user_service.input_user_verification(user[0],user[1])
+    user_name, user_password = user_service.user_input()
+    user_service.create_user(user_name, user_password)
 
-    # Benutzer verifizieren
-
-    #user_service.input_user_verification()
+    # Benutzerverifikation
+    user_service.input_user_verification()
